396f5540a523df6e3aca5e17b7afbcf66ad04c54
Moja One <joalex09@gmail.com>
2021-02-14 18:40:07 -0500

    link data

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,3 +9,19 @@ L.tileLayer(
 	"https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
 		"access_token=pk.eyJ1IjoidmFyc2hhcmFtYWNoIiwiYSI6ImNqbnhyeGJsejAzaXgza28wcjRhNHNjMm0ifQ.H9CPMe-YN9zMVYFJIdB4aA"
 ).addTo(myMap);
+
+// JSON link
+//var API_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var link =
+	"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+d3.json(link, function (data) {
+	var features = data["features"];
+	for (var i = 0; i < features.length; i++) {
+		var geometry = features[i]["geometry"]["coordinates"];
+		var magnitude = features[i]["properties"]["mag"];
+		var place = features[i]["properties"]["place"];
+		var coords = {
+			longitude: geometry["0"],
+			latitude: geometry["1"],
+            depth: geometry["1"],
+		};
