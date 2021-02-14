982eac7dce69d72fcd4deae5fa9b041dea92dad5
Moja One <joalex09@gmail.com>
2021-02-14 18:33:20 -0500

    tile layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,11 @@
-// create map
+// Create map
 var myMap = L.map("map", {
 	center: [37.09, -95.71],
 	zoom: 5,
 });
+
+// Create the tile layer that will be the background of the world map displaying earthquakes
+L.tileLayer(
+	"https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+		"access_token=pk.eyJ1IjoidmFyc2hhcmFtYWNoIiwiYSI6ImNqbnhyeGJsejAzaXgza28wcjRhNHNjMm0ifQ.H9CPMe-YN9zMVYFJIdB4aA"
+).addTo(myMap);
