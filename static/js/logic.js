628ed1b443d9a24713904bc55fa471f49120d4e6
Moja One <joalex09@gmail.com>
2021-02-18 00:11:08 -0500

    js update

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,64 +23,85 @@ d3.json(link, function (data) {
 		var coords = {
 			longitude: geometry["0"],
 			latitude: geometry["1"],
-            depth: geometry["1"],
+            depth: geometry["2"],
 		};
         //Add circles to markers. 
 		var latlng = L.latLng(coords.latitude, coords.longitude);
-		var circle = L.circle(latlng, {
-			color: getColor(magnitude),
-			fillColor: getColor(magnitude),
+        	var circle = L.circleMarker(latlng, {
+			color:"#000000",
+			fillColor: getColor(coords.depth),
 			fillOpacity: 1,
-			radius: magnitude * 15000,
-        })  
-            .addTo(myMap);
-    
-
-        //Add tool tip and pop up information to each earthquake marker
-        //CLICK HAS TO BE ON DEAD CENTER!!
-		L.circle(latlng)
-			.bindPopup(
-				`<h1>${place}</h1> <hr> <h3>Magnitude: ${magnitude}</h3> <h3>Depth: ${coords.depth}</h3>`
-			)
-			.addTo(myMap);
-            }
-            //Add Legend 
-            var legend = L.control({ position: "bottomright" });
-            legend.onAdd = function (myMap) {
-                var div = L.DomUtil.create("div", "info legend"),
-                    colors = ["green", "yellow", "orange", "red"],
-                    labels = ["< 1.0", "1.0 - 2.0", "2.0 - 3.0", "> 3.0"];
-                div.innerHTML += "<h4 style = 'color: #fff'>Magnitude</h4>";
+			radius: magnitude * 4,
         
-                // loop through our density intervals and generate a label with a colored square for each interval
-                for (var i = 0; i < labels.length; i++) {
-                    div.innerHTML +=
-                        '<i style="background:' +
-                        colors[i] +
-                        '">' +
-                        labels[i] +
-                        "</i> <br>";
-                }
-                return div;
-            };
-          legend.addTo(myMap);
+        //Add tool tip and pop up information to each earthquake marker
+        }).bindPopup(
+            `<h1>${place}</h1> <hr> <h3>Magnitude: ${magnitude}</h3> <h3>Depth: ${coords.depth}</h3>`
+        )  
+            .addTo(myMap);
+		
+            }
+            
+            //Add Legend 
+        //     var legend = L.control({ position: "bottomright" });
+        //     legend.onAdd = function (myMap) {
+        //         var div = L.DomUtil.create("div", "info legend"),
+        //             colors = ["green", "yellow", "orange", "red"],
+        //             labels = ["< 1.0", "1.0 - 2.0", "2.0 - 3.0", "> 3.0"];
+        //         div.innerHTML += "<h4 style = 'color: #fff'>Magnitude</h4>";
+        
+        //         // loop through our density intervals and generate a label with a colored square for each interval
+        //         for (var i = 0; i < labels.length; i++) {
+        //             div.innerHTML +=
+        //                 '<i style="background:' +
+        //                 colors[i] +
+        //                 '">' +
+        //                 labels[i] +
+        //                 "</i> <br>";
+        //         }
+        //         return div;
+        //     };
+        //   legend.addTo(myMap);
 
                  
-});          
+ });          
     //Assign colors 
-          function getColor(mag) {
+
+        //   function getColor(mag) {
+        //     var color = "";
+        //     if (mag <= 1) {
+        //         color = "green";
+        //     } else if (mag > 1 && mag <= 2) {
+        //         color = "yellow";
+        //     } else if (mag > 2 && mag <= 3) {
+        //         color = "orange";
+        //     } else if (mag > 3) {
+        //         color = "red";
+        //     }
+        //     return color;  
+        // } 
+        
+        function getColor(depth) {
             var color = "";
-            if (mag <= 1) {
+            if (depth <= -10) {
                 color = "green";
-            } else if (mag > 1 && mag <= 2) {
+            } else if (depth > -10 && depth <= 10) {
                 color = "yellow";
-            } else if (mag > 2 && mag <= 3) {
+            } else if (depth > 10 && depth <= 30) {
                 color = "orange";
-            } else if (mag > 3) {
-                color = "red";
+            } else if (depth > 30 && depth <= 50) {
+                color = "brown";
+            }else if (depth > 50 && depth <= 70) {
+                color = "purple";
+            }else if (depth > 70 && depth <= 90) {
+                    color = "red";   
+            }else if (depth > 90) {
+                color = "black";
             }
-            return color;  
-        }       
+            console.log(depth,color)
+
+                return color;
+         
+        }
 
 
 
